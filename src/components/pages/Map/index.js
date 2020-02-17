e9b87a9c7b412701bfccc751d987e4eaf08f58e2
Luckey <luckeysherpa07@gmail.com>
2020-02-17 22:25:23 +0545

    added RNMaps to the project

--- src/components/pages/Map/index.js
+++ src/components/pages/Map/index.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { 
+import {
   View,
   Text,
   StyleSheet
 } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Map = () => (
   <View style={styles.container}>
-    <Text>Map</Text>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    >
+    </MapView>
   </View>
-  )
+)
 export default Map;
 
 const styles = StyleSheet.create({
@@ -17,5 +28,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   }
 });
